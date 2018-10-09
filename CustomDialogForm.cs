edee4556298127e3099828123f32af3ca4ee6ab4
arnab123456 <43848868+arnab123456@users.noreply.github.com>
2018-10-09 17:19:50 +0530

    Add files via upload

---
+++ CustomDialogForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Demo01
+{
+    public partial class CustomDialogForm : Form
+    {
+        private CustomDialogForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You Pressed OK");
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
